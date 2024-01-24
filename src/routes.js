952abaea8910534cae87a6509308661bf49d36cb
gabrielcbritto <gabrielcbritto@gmail.com>
2024-01-24 11:21:09 -0300

    eslint add

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import {Router} from "express"
+import { Router } from 'express'
 
 const routes = new Router()
 
 routes.get('/', (request, response) => {
-    return response.json({message: "Hello Word"})
+  return response.json({ message: 'Hello Word' })
 })
 
 export default routes
